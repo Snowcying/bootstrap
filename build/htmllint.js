29c2a8ce6388d101433da7f2cb66df0d7e40dd46
XhmikosR <xhmikosr@gmail.com>
2017-10-03 10:03:04 +0300

    (DOESN'T WORK) Ignore more.

--- build/htmllint.js
+++ build/htmllint.js
@@ -21,7 +21,12 @@ childProcess.exec('java -version', function (error) {
   const ignores = [
     'Attribute “autocomplete” is only allowed when the input type is “color”, “date”, “datetime-local”, “email”, “hidden”, “month”, “number”, “password”, “range”, “search”, “tel”, “text”, “time”, “url”, or “week”.',
     'Attribute “autocomplete” not allowed on element “button” at this point.',
-    'Element “img” is missing required attribute “src”.'
+    'Consider using the “h1” element as a top-level heading only (all “h1” elements are treated as top-level headings by many screen readers and other tools).',
+    'Element “img” is missing required attribute “src”.',
+    'Element “legend” not allowed as child of element “div” in this context.',
+    'Element “ul” not allowed as child of element “ul” in this context.',
+    'The “datetime-local” input type is not supported in all browsers. Please be sure to test, and consider using a polyfill.',
+    'The “main” role is unnecessary for element “main”.'
   ]
 
   const args = [
